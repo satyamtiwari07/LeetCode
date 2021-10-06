e1837fad0d3bba9710ed6d1a5025542dbd1c32d1
Satyam Tiwari <89099002+satyamtiwari07@users.noreply.github.com>
2021-10-06 23:12:40 +0530

    Update Contains_Duplicate.cpp

--- Contains_Duplicate.cpp
+++ Contains_Duplicate.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        set<int> s;
+        unordered_set<int> s;
         for(int x=0;x<nums.size();x++){
             if(s.find(nums[x])==s.end()){
                  s.insert(nums[x]);
